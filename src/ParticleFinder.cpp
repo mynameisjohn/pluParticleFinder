1530b8317a7293be6206c87cade896dacf57734e
John Joseph <mynameisjohnj@gmail.com>
2019-11-07 13:27:39 -0500

    Adding code to log the found particles that can be piped to pluLink3dt

--- src/ParticleFinder.cpp
+++ src/ParticleFinder.cpp
@@ -280,11 +280,34 @@ std::vector<ParticleFinder::FoundParticle> ParticleFinder::Execute(std::shared_p
         GetSolver()->Init();
 
         {
+            std::ofstream outputFile ("phi41pct_3D_6zoom_2DCenters.txt", std::ios::out);
+            outputFile.setf (std::ios::fixed);
+            outputFile.precision (1);
+            bool open = outputFile.is_open ();
+
             CudaStopWatch sdf ("DSP");
             // Otherwise we do the DSP with our current params and return found particles
             for (size_t i = 0; i < m_vdInputImages.size (); i++)
             {
-                doDSPAndFindParticlesInImg ((int)i, m_vdInputImages[i], true);
+                int width = m_vdInputImages[0].cols;
+                std::vector<FoundParticle> particlesInImg;
+                doDSPAndFindParticlesInImg ((int)i, m_vdInputImages[i], false, &particlesInImg);
+
+                std::sort (particlesInImg.begin (), particlesInImg.end (), [width](
+                    const FoundParticle& a, const FoundParticle& b)
+                    {
+                        return (a.fPosX + a.fPosY * width) < (b.fPosX + b.fPosY * width);
+                    });
+
+                for (int p = 0; p < particlesInImg.size (); p++)
+                {
+                    outputFile << m_mapImageToStackFrame[i].first << '\t';
+                    outputFile << m_mapImageToStackFrame[i].second << '\t';
+                    outputFile << particlesInImg[p].fPosX << '\t';
+                    outputFile << particlesInImg[p].fPosY << '\t';
+                    outputFile << particlesInImg[p].fIntensity << '\t';
+                    outputFile << sqrt (particlesInImg[p].fR2) << std::endl;
+                }
             }
         }
 
