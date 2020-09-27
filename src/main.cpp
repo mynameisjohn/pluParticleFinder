7dc1e9fa6851905b65a90dadbbd20e0d14d4efc8
John Joseph <mynameisjohnj@gmail.com>
2020-09-27 17:57:23 -0400

    Setting up Unity extern.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#if !defined (PLU_DLL)
 
 #include "ParticleFinder.h"
 
@@ -8,8 +7,94 @@
 #include <map>
 #include <sstream>
 
+// parse XML params and execute
+int parseXMLAndExecute (pugi::xml_document paramDoc)
+{
+    ParticleFinder P;
+    std::list<std::string> inputFiles;
+    int startOfStack{ 0 }, endOfStack{ 0 };
+    bool getUserInput = false;
+    bool linkParticles = false;
+    std::string outputFile2D;
+
+    auto params = paramDoc.child ("pluParams");
+
+    if (!params.attribute ("user_input").empty ())
+        getUserInput = params.attribute ("user_input").as_bool ();
+
+    if (!params.attribute ("link_particles").empty ())
+        linkParticles = params.attribute ("link_particles").as_bool ();
+
+    if (params.attribute ("outputfile_2D"))
+        outputFile2D = params.attribute ("outputfile_2D").as_string ();
+
+    auto inputParams = params.child ("input");
+    startOfStack = inputParams.attribute ("stack_start").as_int ();
+    endOfStack = inputParams.attribute ("stack_end").as_int ();
+
+    for (auto& inputFileEl : inputParams.children ())
+        if (std::strcmp (inputFileEl.name (), "file") == 0)
+            inputFiles.push_back (inputFileEl.child_value ());
+
+    auto cfParams = params.child ("centerfind_params");
+    P.SetGaussianRadius (cfParams.attribute ("filter_radius").as_int ());
+    P.SetDilationRadius (cfParams.attribute ("dilation_radius").as_int ());
+    P.SetFWHM (cfParams.attribute ("HWHM").as_float ());
+    P.SetParticleThreshold (cfParams.attribute ("particle_threshold").as_float ());
+
+    if (linkParticles)
+    {
+        auto linkParams = params.child ("link_params");
+        if (linkParams.attribute ("outputfile_XYZT").as_string ())
+            P.SetOutputFileXYZT (linkParams.attribute ("outputfile_XYZT").as_string ());
+        P.GetSolver ()->SetFeatureRadius (linkParams.attribute ("feature_radius").as_int ());
+        P.GetSolver ()->SetMaskRadius (linkParams.attribute ("mask_radius").as_int ());
+        P.GetSolver ()->SetNeighborRadius (linkParams.attribute ("neighbor_radius").as_int ());
+        P.GetSolver ()->SetMinSliceCount (linkParams.attribute ("min_slices").as_int ());
+        P.GetSolver ()->SetMaxSliceCount (linkParams.attribute ("max_slices").as_int ());
+        P.GetSolver ()->SetXYFactor (linkParams.attribute ("xy_factor").as_float ());
+        P.GetSolver ()->SetZFactor (linkParams.attribute ("z_factor").as_float ());
+    }
+
+    P.Initialize (inputFiles, startOfStack, endOfStack, getUserInput);
+
+    P.SetOutputFile2D (outputFile2D);
+
+    P.Execute (linkParticles);
+
+    return EXIT_SUCCESS;
+}
+
+#ifdef _WIN32
+    #define DLL_EXPORT __declspec(dllexport) 
+#else
+    #define DLL_EXPORT
+#endif
+
+DLL_EXPORT int runFromUnity (const char* xmlString)
+{
+    pugi::xml_document paramDoc;
+    if (!paramDoc.load_string (xmlString))
+        return EXIT_FAILURE;
+
+    return parseXMLAndExecute (std::move (paramDoc));
+}
+
+#if defined (PLU_DLL)
+#else
+
 int main( int argc, char ** argv )
 {
+    if (argc == 2)
+    {
+        pugi::xml_document paramDoc;
+        if (!paramDoc.load_file (argv[1]))
+            return EXIT_FAILURE;
+
+        return parseXMLAndExecute (std::move (paramDoc));
+    }
+
+    // This just runs using the default centerfind args
     ParticleFinder P;
     std::list<std::string> inputFiles;
     int startOfStack{ 0 }, endOfStack{ 0 };
@@ -17,72 +102,28 @@ int main( int argc, char ** argv )
     bool linkParticles = false;
     pugi::xml_document paramDoc;
     std::string outputFile2D;
-
-    if (argc == 2 && paramDoc.load_file (argv[1]))
-    {
-        auto params = paramDoc.child ("pluParams");
-
-        if (!params.attribute ("user_input").empty ())
-            getUserInput = params.attribute ("user_input").as_bool ();
-
-        if (!params.attribute ("link_particles").empty ())
-            linkParticles = params.attribute ("link_particles").as_bool ();
-
-        if (params.attribute("outputfile_2D"))
-            outputFile2D = params.attribute ("outputfile_2D").as_string ();
-
-        auto inputParams = params.child ("input");
-        startOfStack = inputParams.attribute ("stack_start").as_int ();
-        endOfStack = inputParams.attribute ("stack_end").as_int ();
-
-        for (auto& inputFileEl : inputParams.children ())
-            if (std::strcmp (inputFileEl.name (), "file") == 0)
-                inputFiles.push_back (inputFileEl.child_value ());
-
-        auto cfParams = params.child ("centerfind_params");
-        P.SetGaussianRadius (cfParams.attribute ("filter_radius").as_int ());
-        P.SetDilationRadius (cfParams.attribute ("dilation_radius").as_int ());
-        P.SetFWHM (cfParams.attribute ("HWHM").as_float ());
-        P.SetParticleThreshold (cfParams.attribute ("particle_threshold").as_float ());
-
-        if (linkParticles)
-        {
-            auto linkParams = params.child ("link_params");
-            if (linkParams.attribute ("outputfile_XYZT").as_string ())
-                P.SetOutputFileXYZT (linkParams.attribute ("outputfile_XYZT").as_string ());
-            P.GetSolver ()->SetFeatureRadius (linkParams.attribute ("feature_radius").as_int ());
-            P.GetSolver ()->SetMaskRadius (linkParams.attribute ("mask_radius").as_int ());
-            P.GetSolver ()->SetNeighborRadius (linkParams.attribute ("neighbor_radius").as_int ());
-            P.GetSolver ()->SetMinSliceCount (linkParams.attribute ("min_slices").as_int ());
-            P.GetSolver ()->SetMaxSliceCount (linkParams.attribute ("max_slices").as_int ());
-            P.GetSolver ()->SetXYFactor (linkParams.attribute ("xy_factor").as_float ());
-            P.GetSolver ()->SetZFactor (linkParams.attribute ("z_factor").as_float ());
-        }
-    }
-    else if (argc == 6)
-    {
-        std::string infileStem = argv[1];
-        outputFile2D = argv[2];
-        startOfStack = std::atoi (argv[3]);
-        endOfStack = std::atoi (argv[4]);
-        int stackCount = std::atoi (argv[5]);
-
-        for (int i = 0; i < stackCount; i++)
-        {
-            std::stringstream stacknumber_stream;
-            stacknumber_stream.setf (std::ios::right, std::ios::adjustfield);
-            stacknumber_stream.fill ('0');
-            stacknumber_stream << std::setw (4) << i;
-            inputFiles.push_back (infileStem + "_" + stacknumber_stream.str () + ".tif");
-        }
-
-        getUserInput = true;
-    }
-    else 
+    if (argc != 6)
     {
         std::cout << "Invalid params" << std::endl;
         return EXIT_FAILURE;
     }
+        
+    std::string infileStem = argv[1];
+    outputFile2D = argv[2];
+    startOfStack = std::atoi (argv[3]);
+    endOfStack = std::atoi (argv[4]);
+    int stackCount = std::atoi (argv[5]);
+
+    for (int i = 0; i < stackCount; i++)
+    {
+        std::stringstream stacknumber_stream;
+        stacknumber_stream.setf (std::ios::right, std::ios::adjustfield);
+        stacknumber_stream.fill ('0');
+        stacknumber_stream << std::setw (4) << i;
+        inputFiles.push_back (infileStem + "_" + stacknumber_stream.str () + ".tif");
+    }
+
+    getUserInput = true;
 
     P.Initialize (inputFiles, startOfStack, endOfStack, getUserInput);
     
